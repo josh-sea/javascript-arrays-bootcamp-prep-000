429ad632542e789003343b9f43ecafde2f4ebf95
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-08-29 09:30:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,8 @@ function accessElementInArray(array,index) {
   return array[index];
 }
 function destructivelyRemoveElementFromBeginningOfArray(array,element) {
-  newArray = [element,...array]
+  array.shift(element)
+  return array;
   return newArray
 }
 //function takes in paramater array for a, unshifts parameter b as first array element in //paramter a, returns array a
